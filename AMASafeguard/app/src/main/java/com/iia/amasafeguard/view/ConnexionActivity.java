f25f20b04a778b3c16e3e5695847fc6ab49fb858
maximegeoffroy <gemaxime@hotmail.fr>
2016-01-19 16:36:14 +0100

    file architecture modify view activity created

--- AMASafeguard/app/src/main/java/com/iia/amasafeguard/view/ConnexionActivity.java
+++ AMASafeguard/app/src/main/java/com/iia/amasafeguard/view/ConnexionActivity.java
@@ -1,7 +1,10 @@
 package com.iia.amasafeguard.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.iia.amasafeguard.R;
 
@@ -11,5 +14,24 @@ public class ConnexionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_connexion);
+
+        Button btConnexion = (Button)this.findViewById(R.id.btConnexion);
+        Button btInscription = (Button)this.findViewById(R.id.btInscription);
+
+        btConnexion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConnexionActivity.this, SynchronizeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btInscription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ConnexionActivity.this, InscriptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
