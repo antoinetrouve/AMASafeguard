7846c1e785e2e67615476eb518995fb21c59c40a
maximegeoffroy <gemaxime@hotmail.fr>
2016-01-19 14:04:50 +0100

    create android project with file architecture

---
+++ AMASafeguard/app/src/main/java/com/iia/amasafeguard/view/ConnexionActivity.java
@@ -0,0 +1,15 @@
+package com.iia.amasafeguard.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.iia.amasafeguard.R;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_connexion);
+    }
+}
